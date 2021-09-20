5a16689641265890fe5824f03e8ea7d8adc81620
Trevor Gross <t.gross35@gmail.com>
2021-09-20 04:07:05 -0400

    Renamed project to official name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="PyAltium-tgross",
+    name="PyAltium",
     version="0.0.3",
     author="Trevor Gross",
     author_email="t.gross35@gmail.com",
