dee0db0224c1f45365e35218d226d9ce7daa8f7b
Trevor Gross <t.gross35@gmail.com>
2022-01-08 18:16:38 -0500

    More docs

--- docs/conf.py
+++ docs/conf.py
@@ -68,6 +68,21 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # a list of builtin themes.
 #
 html_theme = "sphinx_rtd_theme"
+html_theme_options = {
+    # 'analytics_id': 'G-XXXXXXXXXX',  #  Provided by Google in your dashboard
+    # 'analytics_anonymize_ip': False,
+    # 'prev_next_buttons_location': 'bottom',
+    "style_external_links": True,
+    # 'vcs_pageview_mode': '',
+    # 'style_nav_header_background': 'white',
+    # Toc options
+    # 'collapse_navigation': True,
+    # 'sticky_navigation': True,
+    # 'navigation_depth': 4,
+    # 'includehidden': True,
+    "titles_only": False,
+}
+github_url = "https://github.com/tgross35/pyaltium/"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -75,12 +90,12 @@ html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
 
 
-html_sidebars = {
-    "**": [
-        # "slim_searchbox.html",
-        # "globaltoc.html",
-        # "relations.html",
-        "links.html",
-        # "sourcelink.html",
-    ],
-}
+# html_sidebars = {
+#     "**": [
+#         # "slim_searchbox.html",
+#         # "globaltoc.html",
+#         # "relations.html",
+#         "links.html",
+#         # "sourcelink.html",
+#     ],
+# }
