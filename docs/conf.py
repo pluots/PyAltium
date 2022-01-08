9e77c7512379a434c00beef7375c17d17687b2ee
Trevor Gross <t.gross35@gmail.com>
2022-01-08 18:10:22 -0500

    Test docs sidebar

--- docs/conf.py
+++ docs/conf.py
@@ -73,3 +73,14 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+
+html_sidebars = {
+    "**": [
+        # "slim_searchbox.html",
+        # "globaltoc.html",
+        # "relations.html",
+        "links.html",
+        # "sourcelink.html",
+    ],
+}
